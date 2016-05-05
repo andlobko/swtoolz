d8a1b03474a65381cf9a841ff21576574b2fd45b
andlobko <and.lobko@gmail.com>
2016-05-05 23:39:07 +0300

    Validate subnet_addr: valid&uniq on create, not changed on update.

--- app/models/subnet.rb
+++ app/models/subnet.rb
@@ -1,9 +1,16 @@
 class Subnet < ActiveRecord::Base
-  validate :subnet_should_be_valid
-  #validates_uniqueness_of :subnet_addr
+  validate :subnet_should_be_valid, on: :create
+  validate :subnet_addr_should_not_change, on: :update
+
+  private
 
   def subnet_should_be_valid
     errors.add(:subnet_addr, 'invalid') unless IPAddress.valid?(subnet_addr)
     errors.add(:subnet_addr, 'already exists') if Subnet.find_by(subnet_addr: subnet_addr)
   end
+
+  def subnet_addr_should_not_change
+    errors.add(:subnet_addr, 'should not be changed') if subnet_addr_changed?
+  end
+
 end
