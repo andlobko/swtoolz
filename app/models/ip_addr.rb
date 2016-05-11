52b026db33cc72f8d7ffd7976d7ce07a312295c0
andlobko <and.lobko@gmail.com>
2016-05-11 14:24:21 +0300

    Ad assigned/free ip_addrs count to subnets views.

--- app/models/ip_addr.rb
+++ app/models/ip_addr.rb
@@ -1,6 +1,8 @@
 class IpAddr < ActiveRecord::Base
   belongs_to :subnet, touch: true
   validate :ip_should_not_change, on: :update
+  scope :unassigned, -> { where(description: nil) }
+  scope :assigned,   -> { where.not(description: nil) }
 
   def free
     self.update(description: nil)
