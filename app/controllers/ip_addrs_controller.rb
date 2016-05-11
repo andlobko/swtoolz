aeb079e7f9b912380a7cab10e91509bece4ef0e1
andlobko <and.lobko@gmail.com>
2016-05-11 13:27:08 +0300

    Make ip_addr destroy set description to nil

--- app/controllers/ip_addrs_controller.rb
+++ app/controllers/ip_addrs_controller.rb
@@ -2,39 +2,13 @@ class IpAddrsController < ApplicationController
   before_action :set_ip_addr, only: [:show, :edit, :update, :destroy]
   before_action :set_subnet, only: [:show, :edit, :update, :destroy]
 
-  # GET /ip_addrs
-  # GET /ip_addrs.json
-  def index
-    @ip_addrs = IpAddr.all
-  end
-
   # GET /ip_addrs/1
   # GET /ip_addrs/1.json
   def show
   end
 
-  # GET /ip_addrs/new
-  def new
-    @ip_addr = IpAddr.new
-  end
-
   # GET /ip_addrs/1/edit
   def edit
-  end
-
-  # POST /ip_addrs
-  # POST /ip_addrs.json
-  def create
-    @ip_addr = IpAddr.new(ip_addr_params)
-    respond_to do |format|
-      if @ip_addr.save
-        format.html { redirect_to @ip_addr, notice: 'Ip addr was successfully created.' }
-        format.json { render :show, status: :created, location: @ip_addr }
-      else
-        format.html { render :new }
-        format.json { render json: @ip_addr.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /ip_addrs/1
@@ -54,9 +28,9 @@ class IpAddrsController < ApplicationController
   # DELETE /ip_addrs/1
   # DELETE /ip_addrs/1.json
   def destroy
-    @ip_addr.destroy
+    @ip_addr.free
     respond_to do |format|
-      format.html { redirect_to ip_addrs_url, notice: 'Ip addr was successfully destroyed.' }
+      format.html { redirect_to @subnet, notice: 'Ip addr was successfully set free.' }
       format.json { head :no_content }
     end
   end
--- app/models/ip_addr.rb
+++ app/models/ip_addr.rb
@@ -1,4 +1,16 @@
 class IpAddr < ActiveRecord::Base
   belongs_to :subnet, touch: true
+  validate :ip_should_not_change, on: :update
+
+  def free
+    self.update(description: nil)
+  end
+
+  private
+
+  def ip_should_not_change
+    errors.add(:ip, 'should not be changed') if ip_changed?
+  end
+
 
 end
