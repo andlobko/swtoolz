038f98f1f87e3c49405be7fb016691eb505614a8
andlobko <and.lobko@gmail.com>
2016-06-07 16:25:05 +0300

    Finish logs page styling

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,9 +1,13 @@
 class LogsController < ApplicationController
   def show
     @log_actions = Log.unscoped.select(:action).distinct
+    @log_types = Log.unscoped.select(:auditable_type).distinct
     @logs = Log.unscoped.
         search_action(params[:log_action]).
-        order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+        search_type(params[:log_type]).
+        search_comment(params[:log_comment]).
+        search_changes(params[:log_changes]).
+        order(created_at: :desc).paginate(page: params[:page], per_page: params[:per_page])
     respond_to do |format|
       format.html
       format.js {}
--- app/models/log.rb
+++ app/models/log.rb
@@ -9,5 +9,28 @@ class Log
     end
   end
 
+  def self.search_type(type)
+    if type.present?
+      where(auditable_type: type)
+    else
+      where(nil)
+    end
+  end
+
+  def self.search_comment(comment)
+    if comment.present?
+      where("comment LIKE ?", "%#{comment}%")
+    else
+      where(nil)
+    end
+  end
+
+  def self.search_changes(changes)
+    if changes.present?
+      where("audited_changes LIKE ?", "%#{changes}%")
+    else
+      where(nil)
+    end
+  end
 
 end
